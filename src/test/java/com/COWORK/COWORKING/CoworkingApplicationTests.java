52c55129ea78c974d9a91847e3d739e02599550f
jargoh999 <fatoyeayomide123456@gmail.com>
2024-08-03 13:06:11 +0100

    commit

---
+++ src/test/java/com/COWORK/COWORKING/CoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.COWORK.COWORKING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
