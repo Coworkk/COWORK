dd13f324fd2e7924158700c956a2ac90ba6e380a
jargoh999 <fatoyeayomide123456@gmail.com>
2024-08-03 16:18:41 +0100

    latest commit

---
+++ src/main/java/com/COWORK/COWORKING/repository/Tasks.java
@@ -0,0 +1,7 @@
+package com.COWORK.COWORKING.repository;
+
+import com.COWORK.COWORKING.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Tasks extends JpaRepository<Task,Long> {
+}
