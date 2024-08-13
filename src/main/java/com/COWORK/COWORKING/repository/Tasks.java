580445e86cc10167aeafd1969031dd6ce085942a
jargoh999 <fatoyeayomide123456@gmail.com>
2024-08-13 12:15:14 +0100

    merged ajiri

--- src/main/java/com/COWORK/COWORKING/repository/Tasks.java
+++ src/main/java/com/COWORK/COWORKING/repository/Tasks.java
@@ -1,8 +1,6 @@
 package com.COWORK.COWORKING.repository;
-
 import com.COWORK.COWORKING.data.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface Tasks extends JpaRepository<Task,Long> {
 
 }
