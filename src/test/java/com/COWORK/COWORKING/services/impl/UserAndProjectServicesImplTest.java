16139b247639ffc69f0fcaa378a8763a4556f70d
jargoh999 <fatoyeayomide123456@gmail.com>
2024-08-14 14:58:29 +0100

    commit

---
+++ src/test/java/com/COWORK/COWORKING/services/impl/UserAndProjectServicesImplTest.java
@@ -0,0 +1,35 @@
+package com.COWORK.COWORKING.services.impl;
+
+import com.COWORK.COWORKING.dtos.requests.AddMemberToProjectRequest;
+import com.COWORK.COWORKING.dtos.responses.AddMemberToProjectResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Sql(scripts ={"/database/data.sql"})
+class UserAndProjectServicesImplTest {
+    @Autowired
+    private UserAndProjectServicesImpl userAndProjectServices;
+    @Test
+    @Sql(scripts ={"/database/data.sql"})
+    void addMemberToProject() {
+        AddMemberToProjectRequest projectRequest = new AddMemberToProjectRequest();
+        projectRequest.setMemberId("");
+        projectRequest.setProjectId(200L);
+        AddMemberToProjectResponse response = userAndProjectServices.addMemberToProject(projectRequest);
+        assertThat(response.getProjectUserRoleId()).isEqualTo(1L);
+        assertThat(response.getRoleId()).isNull();
+    }
+    @Test
+    void addSameMemberAgainToProject_ThrowsException() {
+        AddMemberToProjectRequest projectRequest = new AddMemberToProjectRequest();
+        projectRequest.setMemberId("");
+        projectRequest.setProjectId(200L);
+        userAndProjectServices.addMemberToProject(projectRequest);
+        assertThrows(RuntimeException.class, () -> userAndProjectServices.addMemberToProject(projectRequest));
+    }
+}
